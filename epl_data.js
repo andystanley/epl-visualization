e02531c3703f348f39c6aab8b8df74803428b021
Andrew Stanley <andystanley@hotmail.ca>
2017-11-02 14:43:43 -0400

    Added data files and the default map

---
+++ epl_data.js
@@ -0,0 +1,239 @@
+eqfeed_callback({
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -71.542969,-35.675147 ]
+    },
+    "properties": {
+    "name":"Alexis Sanchez",
+    "club":"Arsenal",
+    "age":28,
+    "position":"LW",
+    "position_cat":1,
+    "market_value":65,
+    "page_views":4329,
+    "fpl_value":12,
+    "fpl_sel":"17.10%",
+    "fpl_points":264,
+    "region":3,
+    "nationality":"Chile",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.451526,51.165691 ]
+    },
+    "properties": {
+    "name":"Mesut Ozil",
+    "club":"Arsenal",
+    "age":28,
+    "position":"AM",
+    "position_cat":1,
+    "market_value":50,
+    "page_views":4395,
+    "fpl_value":9.5,
+    "fpl_sel":"5.60%",
+    "fpl_points":167,
+    "region":2,
+    "nationality":"Germany",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 15.472962,49.817492 ]
+    },
+    "properties": {
+    "name":"Petr Cech",
+    "club":"Arsenal",
+    "age":35,
+    "position":"GK",
+    "position_cat":4,
+    "market_value":7,
+    "page_views":1529,
+    "fpl_value":5.5,
+    "fpl_sel":"5.90%",
+    "fpl_points":134,
+    "region":2,
+    "nationality":"Czech Republic",
+    "new_foreign":"0",
+    "age_cat":6,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -1.1743197,52.3555177 ]
+    },
+    "properties": {
+    "name":"Theo Walcott",
+    "club":"Arsenal",
+    "age":28,
+    "position":"RW",
+    "position_cat":1,
+    "market_value":20,
+    "page_views":2393,
+    "fpl_value":7.5,
+    "fpl_sel":"1.50%",
+    "fpl_points":122,
+    "region":1,
+    "nationality":"England",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.213749,46.227638 ]
+    },
+    "properties": {
+    "name":"Laurent Koscielny",
+    "club":"Arsenal",
+    "age":31,
+    "position":"CB",
+    "position_cat":3,
+    "market_value":22,
+    "page_views":912,
+    "fpl_value":6,
+    "fpl_sel":"0.70%",
+    "fpl_points":121,
+    "region":2,
+    "nationality":"France",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -3.74922,40.463667 ]
+    },
+    "properties": {
+    "name":"Hector Bellerin",
+    "club":"Arsenal",
+    "age":22,
+    "position":"RB",
+    "position_cat":3,
+    "market_value":30,
+    "page_views":1675,
+    "fpl_value":6,
+    "fpl_sel":"13.70%",
+    "fpl_points":119,
+    "region":2,
+    "nationality":"Spain",
+    "new_foreign":"0",
+    "age_cat":2,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.213749,46.227638 ]
+    },
+    "properties": {
+    "name":"Olivier Giroud",
+    "club":"Arsenal",
+    "age":30,
+    "position":"CF",
+    "position_cat":1,
+    "market_value":22,
+    "page_views":2230,
+    "fpl_value":8.5,
+    "fpl_sel":"2.50%",
+    "fpl_points":116,
+    "region":2,
+    "nationality":"France",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -3.74922,40.463667 ]
+    },
+    "properties": {
+    "name":"Nacho Monreal",
+    "club":"Arsenal",
+    "age":31,
+    "position":"LB",
+    "position_cat":3,
+    "market_value":13,
+    "page_views":555,
+    "fpl_value":5.5,
+    "fpl_sel":"4.70%",
+    "fpl_points":115,
+    "region":2,
+    "nationality":"Spain",
+    "new_foreign":"0",
+    "age_cat":4,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"0"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 10.451526,51.165691 ]
+    },
+    "properties": {
+    "name":"Shkodran Mustafi",
+    "club":"Arsenal",
+    "age":25,
+    "position":"CB",
+    "position_cat":3,
+    "market_value":30,
+    "page_views":1877,
+    "fpl_value":5.5,
+    "fpl_sel":"4.00%",
+    "fpl_points":90,
+    "region":2,
+    "nationality":"Germany",
+    "new_foreign":"0",
+    "age_cat":3,
+    "club_id":"1",
+    "big_club":"1",
+    "new_signing":"1"
+    }
+  }
+]
+});
